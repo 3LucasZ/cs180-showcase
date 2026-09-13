0e7bc84bfff9d7098dc8ae7645bf01be42ebbec2
3LucasZ <lucas.zheng@warriorlife.net>
2026-09-13 14:22:32 -0700

    stuff

---
+++ p1/p1-downsample.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+IN_DIR = DIR / "in"
+OUT_DIR = DIR / "out"
+
+
+def downsample2(im):
+    # crop image to be even size before downsampling by 2
+    if (im.shape[0] % 2 == 1):
+        im = im[1:, :]
+    if (im.shape[1] % 2 == 1):
+        im = im[:, 1:]
+    return (im[0::2, 0::2] + im[0::2, 1::2] + im[1::2, 0::2] + im[1::2, 1::2]) / 4.0
+
+
+def downsample2gauss(im):
+    im = cv2.pyrDown(im)
+    return im
+
+
+IN_PATH = IN_DIR / f"sample{1}" / "cathedral.jpg"
+OUT_PATH = OUT_DIR / f"test" / "cathedral_ds_cmp.jpg"
+im = cv2.imread(IN_PATH, cv2.IMREAD_GRAYSCALE)
+height = np.floor(im.shape[0] / 3.0).astype(np.uint)
+g = im[height: 2*height]
+l = downsample2(downsample2(g))
+r = downsample2gauss(downsample2gauss(g))
+im_out = np.hstack([l, r])
+cv2.imwrite(OUT_PATH, im_out)
